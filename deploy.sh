6aebd5d6237d17864e4f5dfdc3fb85b327c2b2c6
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-09-12 16:46:45 +0900

    sudo -u isucon make

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -x
 cd /home/isucon/isuumo/webapp/go
 sudo -u isucon git pull origin main
 systemctl stop isuumo.go.service
-make all
+sudo -u isucon make all
 systemctl restart isuumo.go.service
 systemctl reload nginx
