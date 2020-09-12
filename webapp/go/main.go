be7c49fbd9e2c087ddf23ae2da8f676908b7b93b
yashigani <yashigani_w@hatena.ne.jp>
2020-09-12 18:08:23 +0900

    Add log

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -902,6 +902,21 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 		c.Logger().Errorf("Database execution error : %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
+	fmt.Println("new query: ", len(estates))
+
+	w := chair.Width
+	h := chair.Height
+	d := chair.Depth
+	query = `SELECT * FROM estate WHERE (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) ORDER BY popularity DESC, id ASC LIMIT ?`
+	err = db.SelectContext(ctx, &estates, query, w, h, w, d, h, w, h, d, d, w, d, h, Limit)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
+		}
+		c.Logger().Errorf("Database execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	fmt.Println("old query: ", len(estates))
 
 	return c.JSON(http.StatusOK, EstateListResponse{Estates: estates})
 }
